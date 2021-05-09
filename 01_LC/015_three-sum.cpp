1e9dc4360339990e44a5ce5c30ba3d535aa88807
Gmrakari <gmrakari@outlook.com>
2021-05-09 16:32:41 +0800

    015_three-sum.cpp

--- 01_LC/015_three-sum.cpp
+++ 01_LC/015_three-sum.cpp
@@ -75,3 +75,48 @@ public:
 		return ans;
 	}
 };
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size() < 3)
+            return {};
+
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i < nums.size();i++)
+        {
+            if(nums[i] > 0)
+            {
+                return res;
+            }
+            if( i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while(right > left)
+            {
+                if(nums[i] + nums[left] + nums[right] > 0)
+                {
+                    right--;
+                }
+                else if(nums[i] + nums[left] + nums[right] < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    res.push_back(vector<int>{nums[i],nums[left],nums[right]});
+                    while(right > left && nums[right] == nums[right - 1] ) right--;
+                    while(right > left && nums[left] == nums[left + 1] ) left++;
+
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+};
