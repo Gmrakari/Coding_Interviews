0fece7468c0ede1e360efd51df93f2d2559357ff
m_Eveberry <35594384+Gmrakari@users.noreply.github.com>
2021-05-18 16:46:14 +0800

    Update 098_validate-binary-search-tree.cpp

--- 01_LC/098_validate-binary-search-tree.cpp
+++ 01_LC/098_validate-binary-search-tree.cpp
@@ -152,3 +152,84 @@ public:
         inorder(root->right,vec);
     }
 };
+
+
+/*
+ * Date:2021-05-18 16:45
+ * file create on class 
+ */
+
+#include <bits/stdc++.h>
+/*
+ * 思路：用一个vector保存树的节点
+ * 如果vec的前一个节点大于后一个节点，那么返回false
+ * 否则 返回true
+ */
+ 
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root)
+    {
+        if(root == nullptr)
+            return false;
+        vector<int> vec;
+        inorder(root,vec);
+
+        for(int i = 1;i < vec.size();i++)
+        {
+            if(vec[i] < vec[i - 1])
+                return false;
+        }
+        return true;
+
+    }
+
+    void inorder(TreeNode* root,vector<int> &vec)
+    {
+        if(root == nullptr)
+            return ;
+
+        inorder(root->left,vec);
+        vec.push_back(root->val);
+        inorder(root->right,vec);
+    }
+};
+
+int main()
+{
+	/*
+	TreeNode* node3 = new TreeNode(3,nullptr,nullptr);
+	TreeNode* node6 = new TreeNode(6,nullptr,nullptr);
+	
+	TreeNode* node1 = new TreeNode(1,nullptr,nullptr);
+	TreeNode* node4 = new TreeNode(4,node3,node6);
+	TreeNode* node5 = new TreeNode(5,node1,node4);
+
+	TreeNode* pRoot = node5;
+	*/
+	
+	TreeNode* node1 = new TreeNode(1,nullptr,nullptr);
+	TreeNode* node3 = new TreeNode(3,nullptr,nullptr);
+	TreeNode* node2 = new TreeNode(2,node1,node3);
+	
+	TreeNode* pRoot = node2;
+	
+	Solution bstree;
+	bool ret = bstree.isValidBST(pRoot);
+	
+	cout <<"ret : " << ret << endl;
+	
+	
+}
