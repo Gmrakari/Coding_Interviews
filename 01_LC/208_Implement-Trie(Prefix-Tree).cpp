016b76265122298d0d199d5f7e6cdf7be106ec7b
Gmrakari <gmrakari@outlook.com>
2021-05-28 10:26:23 +0800

    update 208_Implement-Trie(Prefix-Tree).cpp

--- 01_LC/208_Implement-Trie(Prefix-Tree).cpp
+++ 01_LC/208_Implement-Trie(Prefix-Tree).cpp
@@ -13,44 +13,44 @@ private:
 public:
     /** Initialize your data structure here. */
     Trie() {
-			isEnd = false;
-			memeset(next, 0, sizeof(next));
+		isEnd = false;
+		memeset(next, 0, sizeof(next));
     }
 
     /** Inserts a word into the trie. */
     void insert(string word) {
-				Trie* node = this;
-				for (char c : word) {
-					if (node->next[c - 'a'] == NULL) {
-						node->next[c - 'a'] = new Trie();
-					}
-					node = node->next[c - 'a'];
-				}
-				node->isEnd = true;
+		Trie* node = this;
+		for (char c : word) {
+			if (node->next[c - 'a'] == NULL) {
+				node->next[c - 'a'] = new Trie();
+			}
+			node = node->next[c - 'a'];
+		}
+		node->isEnd = true;
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
-			Trie* node = this;
-			for (char c : word) {
-				node = node->next[c - 'a'];
-				if (node == NULL) {
-					return false;
-				}
+		Trie* node = this;
+		for (char c : word) {
+			node = node->next[c - 'a'];
+			if (node == NULL) {
+				return false;
 			}
-			return node->isEnd;
+		}
+		return node->isEnd;
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-			Trie* node = this;
-			for (char c : prefix) {
-				node = node->next[c - 'a'];
-				if (node == NULL) {
-					return false;
-				}
+		Trie* node = this;
+		for (char c : prefix) {
+			node = node->next[c - 'a'];
+			if (node == NULL) {
+				return false;
 			}
-			return true;
+		}
+		return true;
     }
 };
 
