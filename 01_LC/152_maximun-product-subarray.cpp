a4c06b1749a5d1b1dfac3cb58f534f2bbaf32189
Gmrakari <gmrakari@outlook.com>
2021-06-05 15:12:31 +0800

    update 152_maximun-product-subarray.cpp

--- 01_LC/152_maximun-product-subarray.cpp
+++ 01_LC/152_maximun-product-subarray.cpp
@@ -6,16 +6,20 @@
  *
  */
 
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         if (nums.size() == 0) return 0;
-        int len = nums.size();
-        int dp[len][2];
+        int dp[nums.size()][2];
         dp[0][0] = nums[0];
         dp[0][1] = nums[0];
         int res = nums[0];
-        for (int i = 1; i < nums.size();i++) {
+        for (int i = 1; i < (int)nums.size(); i++) {
             int x = i % 2;
             int y = (i - 1) % 2;
             dp[x][0] = max(max(dp[y][0] * nums[i], dp[y][1] * nums[i]),nums[i]);
@@ -26,3 +30,9 @@ public:
 
     }
 };
+
+int main() {
+	Solution su;
+	vector<int> vec = {-2,1,-3};
+	cout <<su.maxProduct(vec) << endl;
+}
