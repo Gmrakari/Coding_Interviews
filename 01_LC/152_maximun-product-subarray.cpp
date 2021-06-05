f3d26da69eac515ad5bb53f65353db4a0ac989f4
Gmrakari <gmrakari@outlook.com>
2021-06-05 15:08:39 +0800

    152_maximun-product-subarray.cpp

---
+++ 01_LC/152_maximun-product-subarray.cpp
@@ -0,0 +1,28 @@
+
+
+/*
+ * Date:2021-06-05 15:08
+ * filename:152_maximun-product-subarray.cpp
+ *
+ */
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int len = nums.size();
+        int dp[len][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.size();i++) {
+            int x = i % 2;
+            int y = (i - 1) % 2;
+            dp[x][0] = max(max(dp[y][0] * nums[i], dp[y][1] * nums[i]),nums[i]);
+            dp[x][1] = min(min(dp[y][0] * nums[i], dp[y][1] * nums[i]),nums[i]);
+            res = max(res, dp[x][0]);
+        }
+        return res;
+
+    }
+};
