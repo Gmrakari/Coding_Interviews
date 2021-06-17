39168ee2f002882f6c51d30700abd2917efb6710
浮海拉卡里 <35594384+Gmrakari@users.noreply.github.com>
2021-06-17 11:56:56 +0800

    Update main.cpp

--- 04_List/02_链表集合测试/main.cpp
+++ 04_List/02_链表集合测试/main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include "linklist.h"
+
 using std::cout;
 using std::endl;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
-	ListNode *node5 = new ListNode(5,NULL);
-	ListNode *node4 = new ListNode(4,node5);
-	ListNode *node3 = new ListNode(3,node4);
-	ListNode *node2 = new ListNode(2,node3);
-	ListNode *node1 = new ListNode(1,node2);
-	ListNode *head = node1;
+
 	
 	ListNode* removeElements(ListNode* head, int val);
 	ListNode* reverse(ListNode* head);
+	ListNode* init();
 	
-	
-	head = removeElements(head,0);
+	ListNode *head = init();
+	head = removeElements(head,2);
 	cout << "remove:";
 	while(head) {
 		cout << head->val << " ";
@@ -25,14 +21,21 @@ int main(int argc, char** argv) {
 	}
 	cout << endl;
 	
-	head = node1;
+	head = init();
 	head = reverse(head);
 	cout << "reverse:";
 	while(head) {
 		cout << head->val << " ";
 		head = head->next;
 	}
+	cout << endl;
+		
+	head = init();
+	cout << "original:";
+	while(head) {
+		cout << head->val << " ";
+		head = head->next;
+	}
 	cout << endl;	
-	
 	return 0;
 }
