f0516000b7826246dfcf116a210e23a6dc62c973
浮海拉卡里 <35594384+Gmrakari@users.noreply.github.com>
2021-06-17 11:56:39 +0800

    Update linklist.cpp

--- 04_List/02_链表集合测试/linklist.cpp
+++ 04_List/02_链表集合测试/linklist.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
 #include "linklist.h"
-using std::cout;
-using std::endl;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	
+ListNode* init() {
 	ListNode *node5 = new ListNode(5,NULL);
 	ListNode *node4 = new ListNode(4,node5);
 	ListNode *node3 = new ListNode(3,node4);
@@ -13,26 +8,51 @@ int main(int argc, char** argv) {
 	ListNode *node1 = new ListNode(1,node2);
 	ListNode *head = node1;
 	
-	ListNode* removeElements(ListNode* head, int val);
-	ListNode* reverse(ListNode* head);
-	
-	
-	head = removeElements(head,0);
-	cout << "remove:";
-	while(head) {
-		cout << head->val << " ";
-		head = head->next;
-	}
-	cout << endl;
-	
-	head = node1;
-	head = reverse(head);
-	cout << "reverse:";
-	while(head) {
-		cout << head->val << " ";
-		head = head->next;
-	}
-	cout << endl;	
-	
-	return 0;
+	return head;
 }
+
+ListNode* removeElements(ListNode* head, int val) {
+	if (head == NULL) return NULL;
+	
+	ListNode *newhead = new ListNode();
+	newhead->next = head;
+	
+	ListNode *pre = newhead;
+	ListNode *cur = newhead->next;
+	
+	while(cur) {
+		if (cur->val == val) {
+			pre->next = cur->next;
+			cur = pre->next;
+		}else {
+			pre = pre->next;
+			cur = cur->next;
+		}
+	}
+	return newhead->next;
+}
+
+ListNode* reverse(ListNode* head) {
+
+/*
+	if (head == NULL) return NULL;
+	stack<ListNode *> st;
+	
+	while(head) {
+		st.push(head);
+		head = head->next;
+	}	
+	
+	ListNode *newhead = st.top();
+	ListNode *newnode = newhead;
+	st.pop();
+	
+	while(!st.empty()) {
+		newnode->next = st.top();
+		st.pop();
+		newnode = newnode->next;
+	}
+	newnode->next = NULL;
+	return newhead;
+*/
+} 
