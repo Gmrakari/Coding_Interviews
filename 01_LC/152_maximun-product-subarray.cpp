e8c9c2da23dd35ec617afbce888ff26476394b97
Gmrakari <gmrakari@outlook.com>
2021-06-19 21:13:09 +0800

    update 152_maximun-product-subarray.cpp

--- 01_LC/152_maximun-product-subarray.cpp
+++ 01_LC/152_maximun-product-subarray.cpp
@@ -31,8 +31,26 @@ public:
     }
 };
 
+class Solution_2 {
+public:
+	int maxProduct(vector<int>& nums) {
+		vector<int> maxF(nums), minF(nums);
+		for (int i = 1; i <(int)nums.size(); ++i) {
+			maxF[i] = max(maxF[i - 1] * nums[i], max(nums[i], minF[i - 1] * nums[i]));
+			minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));
+		}
+		return *max_element(maxF.begin(), maxF.end());
+	}
+};
+
 int main() {
 	Solution su;
 	vector<int> vec = {-2,1,3,4};
+	cout << "Solution_1:";
 	cout <<su.maxProduct(vec) << endl;
+
+	cout << "Solution_2:";
+	Solution_2 so;
+	cout << so.maxProduct(vec) << endl;
+	return 0;
 }
