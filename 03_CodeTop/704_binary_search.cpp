3d8c45bfe599044477b342f9e7771dab1b13a688
Gmrakari <gmrakari@outlook.com>
2021-06-24 19:51:23 +0800

    update sqrt_x

--- 03_CodeTop/704_binary_search.cpp
+++ 03_CodeTop/704_binary_search.cpp
@@ -25,3 +25,34 @@ public:
         return -1;
     }
 };
+
+/*
+ * NC32 求平方根
+ * https://www.nowcoder.com/practice/09fbfb16140b40499951f55113f2166c
+ * Date:2021-06-24 19:50
+ *
+ */
+
+class Solution_2 {
+public:
+    /**
+     *
+     * @param x int整型
+     * @return int整型
+     */
+    int sqrt(int x) {
+        // write code here
+        if (x == 0 || x == 1) return x;
+        long low = 0;
+        long high = x;
+        long mid = 0;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (mid * mid == x) return (int) mid;
+            else if (mid * mid < x) low = (int)(mid + 1);
+            else high = (int)(mid - 1);
+        }
+       return high;
+    }
+};
