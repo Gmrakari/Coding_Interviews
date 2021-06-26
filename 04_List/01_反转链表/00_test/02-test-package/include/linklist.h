d54d7996e823478cfd04e0a64a4cda2adc94c3d7
Gmrakari <gmrakari@outlook.com>
2021-06-26 22:47:44 +0800

    linklist.h

--- 04_List/01_反转链表/00_test/02-test-package/include/linklist.h
+++ 04_List/01_反转链表/00_test/02-test-package/include/linklist.h
@@ -7,7 +7,7 @@
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode() : val(0),next(NULL) {} 
+	ListNode() : val(),next(NULL) {} 
 	ListNode(int x) : val(x),next(NULL) {} 
 	ListNode(int x,ListNode *next) : val(x),next(next) {} 
 };
