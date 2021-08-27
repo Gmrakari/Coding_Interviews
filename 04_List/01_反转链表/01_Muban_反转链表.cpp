132e6306dc4b43d67cb6a2169a63493e834b00ba
Gmrakari <gmrakari@outlook.com>
2021-08-27 23:38:48 +0800

    01_Muban_反转链表.cpp

--- 04_List/01_反转链表/01_Muban_反转链表.cpp
+++ 04_List/01_反转链表/01_Muban_反转链表.cpp
@@ -1,70 +1,63 @@
+
+/*
+ * Date:2021-08-27 23:29
+ * filename:00_Muban.cpp
+ *
+ */
+
 #include <iostream>
 using namespace std;
 
 struct ListNode
 {
-	int 	m_nKey;
+	int 	m_data;
 	ListNode *m_pNext;
+	ListNode() : m_data(0), m_pNext(NULL) {}
+	ListNode(int m_data) : m_data(m_data), m_pNext(NULL) {}
+	ListNode(int m_data, ListNode *m_pNext) : m_data(m_data), m_pNext(m_pNext) {}
 };
 
 ListNode* ReverseList(ListNode* pHead)
 {
+	 if (pHead == NULL) 
+		 return NULL;
 
+	 ListNode *pReverHead = NULL;
+	 ListNode *pNode = pHead;
+	 ListNode *pPrev = NULL;
+
+	 while (pNode != NULL) {
+		 ListNode *pNext = pNode->m_pNext;
+		 if (pNext == NULL) 
+			 pReverHead = pNode;
+		 pNode->m_pNext = pPrev;
+		 pPrev = pNode;
+		 pNode = pNext;
+	 }
+	 return pReverHead;
 }
 
-
-ListNode* ReverseList_2(ListNode* pHead)
-{
-
+ListNode* init() {
+	ListNode *node5 = new ListNode(5, NULL);
+	ListNode *node4 = new ListNode(4, node5);
+	ListNode *node3 = new ListNode(3, node4);
+	ListNode *node2 = new ListNode(2, node3);
+	ListNode *node1 = new ListNode(1, node2);
+	ListNode *pHead = node1;
+	return pHead;
 }
-
 
 int main(int argc,const char* argv[])
 {
-	ListNode node[6];
-	node[0].m_nKey = 0;
-	node[0].m_pNext = &node[1];
 
-	node[1].m_nKey = 1;
-	node[1].m_pNext = &node[2];
-
-	node[2].m_nKey = 2;
-	node[2].m_pNext = &node[3];
-
-	node[3].m_nKey = 3;
-	node[3].m_pNext = &node[4];
-
-	node[4].m_nKey = 4;
-	node[4].m_pNext = &node[5];
-
-	node[5].m_nKey = 5;
-	node[5].m_pNext = NULL;
-
-	ListNode* ReverseList(ListNode* pHead);
-	//ListNode* ReverseList_2(ListNode* pHead);
-
-
-	ListNode *res1,*res2;
-//	res1 = ReverseList(node);
-	res2 = ReverseList_2(node);
-
-	/*
+	ListNode *res1 = init();
+	res1 = ReverseList(res1);
 	while(res1 != NULL)
 	{
-		cout << res1->m_nKey << " ";
+		cout << res1->m_data << " ";
 		res1 = res1->m_pNext;
 	}
 	cout << endl;
-	*/
-
-
-	while(res2 != NULL)
-	{
-		cout << res2->m_nKey << " ";
-		res2 = res2->m_pNext;
-	}
-	cout << endl;
-	
 	return 0;
 }
 
