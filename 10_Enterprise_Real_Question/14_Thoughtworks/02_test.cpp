039b7e5275e351f1183e4bad6dd6e243f74752f6
Gmrakari <gmrakari@outlook.com>
2021-09-25 08:39:12 +0800

    02_test.cpp

---
+++ 10_Enterprise_Real_Question/14_Thoughtworks/02_test.cpp
@@ -0,0 +1,23 @@
+
+
+/*
+ * Date:2021-09-23 12:55
+ * filename:02_test.cpp
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a = 2;
+
+	if (a & 1) {
+		a = 1;
+		cout << "a shi jishu : " << a << endl;
+	}
+	else {
+		cout << "a shi oushu" << a << endl;
+	}
+
+}
