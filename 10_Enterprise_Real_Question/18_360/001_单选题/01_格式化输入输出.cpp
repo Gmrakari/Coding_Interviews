4f4a6fc9f9f725452cf54125300f9e4475b4eb97
Gmrakari <gmrakari@outlook.com>
2021-10-12 21:21:39 +0800

    01_格式化输入输出.cpp

---
+++ 10_Enterprise_Real_Question/18_360/001_单选题/01_格式化输入输出.cpp
@@ -0,0 +1,15 @@
+
+
+/*
+ * Date:2021-10-12 09:22
+ * filename:01_格式化输入输出.cpp
+ *
+ */
+
+#include <stdio.h>
+int main() {
+	int x;
+	float y;
+	scanf("%3d%f", &x, &y);
+	printf("x:%d\ny:%f\n",x,y);
+}
