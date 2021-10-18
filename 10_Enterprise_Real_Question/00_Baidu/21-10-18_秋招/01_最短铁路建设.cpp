88f8b057948c0621fbccbd058ef5b502894422e2
Gmrakari <gmrakari@outlook.com>
2021-10-18 21:18:53 +0800

    01_最短铁路建设.cpp

---
+++ 10_Enterprise_Real_Question/00_Baidu/21-10-18_秋招/01_最短铁路建设.cpp
@@ -0,0 +1,20 @@
+
+
+/*
+ * Date:2021-10-18 20:20
+ * filename:01_最短铁路建设.cpp
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	vector<int> res;
+	for (int i = 0;i < n;i++) {
+
+	}
+	return 0;
+}
