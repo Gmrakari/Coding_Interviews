55aebd7337f26673077c1f6b9de5efc29e525f10
Gmrakari <gmrakari@outlook.com>
2021-12-01 16:24:03 +0800

    15_02_String_的构造函数.cpp

---
+++ 11_C++_basic/15_02_String_的构造函数.cpp
@@ -0,0 +1,50 @@
+
+
+/*
+ * Date:2021-12-01 15:47
+ * filename:15_02_String_的构造函数.cpp
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class String {
+public:
+	String(const char* cstr = 0);
+	String(const String& str);
+	String& operator=(const String& str);
+
+private:
+	char* m_data;
+};
+
+inline String::String(const char* cstr) {
+	if (cstr) {
+		m_data = new char[strlen(cstr) + 1];
+		strcpy(m_data, cstr);
+
+	} else {
+		m_data = new char[1];
+		*m_data = '\0';
+	}
+}
+
+inline String::String(const String& str) {
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy(m_data, str.m_data);
+}
+
+inline String& String::operator=(const String& str) {
+	if (this == &str)
+		return *this;
+
+	delete[] m_data;
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy(m_data, str.m_data);
+
+	return *this;
+}
+
