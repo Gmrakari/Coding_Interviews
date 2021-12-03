11152392af761a643d73e97aafb42364675dcf85
Gmrakari <gmrakari@outlook.com>
2021-12-03 19:29:35 +0800

    add thread 安全和不安全

--- 10_Enterprise_Real_Question/19_珠_安_联_锐_视/05_单例模式.cpp
+++ 10_Enterprise_Real_Question/19_珠_安_联_锐_视/05_单例模式.cpp
@@ -6,6 +6,17 @@
  *
  */
 
+/*
+ * Singleton 的动机(Motivation)
+ *
+ * 在软件系统中，经常有这样一些特殊的类，必须保证它们在系统中只存在一个实例，
+ * 才能确保它们的逻辑正确性、以及良好的效率
+ *
+ * 模式定义：
+ * 保证一个类仅有一个实例，并提供一个该实例的全局访问点
+ *
+ */
+
 #include <bits/stdc++.h>
 
 class Single {
@@ -55,3 +66,21 @@ Singleton* Singleton::getinstance() {
 	return m_instance;
 }
 
+//C++11 volatile
+std::atomic<Singleton*> Singleton::m_instance;
+std::mutex Singleton::m_mutex;
+
+Singleton* Singleton::getinstance() {
+	Singleton* tmp = m_instance.load(std::memory_order_relaxed);
+	std::atomic_thread_fence(std::memory_order_acquire);//获取内存fence
+	if (tmp == nullptr) {
+		std::lock_guard<std::mutex> lock(mutex);
+		tmp = m_instance.load(std:memory_order_relaxed);
+		if (tmp == nullptr) {
+			tmp = new Singleton;
+			std::atomic_thread_fence(std::memory_order_release);
+			m_instance.store(tmp, std::memory_order_relaxed);
+		}
+	}
+	return tmp;
+}
