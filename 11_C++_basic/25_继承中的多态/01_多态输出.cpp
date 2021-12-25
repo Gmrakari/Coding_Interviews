caf17f963073758a22222cda0aacb69449ad51dc
Gmrakari <gmrakari@outlook.com>
2021-12-25 22:57:18 +0800

    01_多态输出.cpp

---
+++ 11_C++_basic/25_继承中的多态/01_多态输出.cpp
@@ -0,0 +1,56 @@
+
+
+/*
+ * Date:2021-12-25 22:50
+ * filename:01_多态输出.cpp
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class animal {
+protected:
+	int age;
+public:
+	virtual void print_age(void) = 0;
+};
+
+class dog : public animal {
+public:
+	dog() {
+		this->age = 2;
+	}
+	~dog() {
+
+	}
+
+	virtual void print_age(void) {
+		cout << "Wang, my age = " << this->age << endl;
+	}
+};
+
+class cat : public animal {
+public:
+	cat() {
+		this->age = 1;
+	}
+	~cat() {
+
+	}
+	virtual void print_age(void) {
+		cout << "Miao, my age = " << this->age << endl;
+	}
+};
+
+int main() {
+	cat kitty;
+	dog jd;
+	animal *pa;
+	int *p = (int*)(&kitty);
+	int *q = (int*)(&jd);
+	p[0] = q[0];
+	pa = &kitty;
+	pa->print_age();
+	return 0;
+}
