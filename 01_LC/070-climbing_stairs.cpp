9004eaca4223b80baea08d95254fc699b8c49193
Gmrakari <gmrakari@outlook.com>
2022-03-18 11:48:38 +0800

    update code format

--- 01_LC/070-climbing_stairs.cpp
+++ 01_LC/070-climbing_stairs.cpp
@@ -9,17 +9,17 @@
 using namespace std;
 
 class Solution {
-public:
-    int climbStairs(int n) {
-				if(n == 0 || n == 1 || n == 2) return n;
-				int res[n];
-				res[0] = 1;
-				res[1] = 2;
-				for(int i = 2; i < n; ++i) {
-					res[i] = res[i - 1] + res[i - 2];
-				}
-				return res[n - 1];
-    }
+	public:
+		int climbStairs(int n) {
+			if(n == 0 || n == 1 || n == 2) return n;
+			int res[n];
+			res[0] = 1;
+			res[1] = 2;
+			for(int i = 2; i < n; ++i) {
+				res[i] = res[i - 1] + res[i - 2];
+			}
+			return res[n - 1];
+		}
 };
 
 int main() {
