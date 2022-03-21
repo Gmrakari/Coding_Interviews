e9ff7c22621026e5578d7b4d8ebac2bab9850fbb
Gmrakari <gmrakari@outlook.com>
2022-03-21 23:45:38 +0800

    01_reverse_list.cpp

---
+++ 10_Enterprise_Real_Question/27_安信服/01_reverse_list.cpp
@@ -0,0 +1,125 @@
+
+
+/*
+ * Date:2022-03-21 23:16
+ * filename:01_reverse_list.cpp
+ *
+ */
+
+#include <iostream>
+
+class ListNode {
+public:
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(NULL){}
+	ListNode(int val, ListNode *next): val(val), next(next){}
+};
+
+class Solution {
+public:
+	ListNode* reverse(ListNode* phead) {
+		if (phead == NULL) return NULL;
+		ListNode* temp;
+		ListNode* cur = phead;
+		ListNode* prev = NULL;
+
+		while (cur) {
+			temp = cur->next;
+			cur->next = prev;
+			prev = cur;
+			cur = temp;
+		}
+		return prev;
+	}
+
+	ListNode* m_reverse(ListNode *prev, ListNode *cur) {
+		if (cur == NULL) 
+			return prev;
+		ListNode *tmp = cur->next;
+		cur->next = prev;
+		return m_reverse(cur, tmp);
+	}
+
+	ListNode* reverselist(ListNode* phead) {
+		return m_reverse(NULL, phead);
+	}
+
+	ListNode* recursion_reverselist(ListNode* phead) {
+		if (phead == NULL)
+			return NULL;
+
+		if (phead->next == NULL)
+			return phead;
+
+		ListNode *tmp = recursion_reverselist(phead->next);
+		phead->next->next = phead;
+		phead->next = NULL;
+
+		return tmp;
+	}
+
+	void traval(ListNode* phead) {
+		if (phead == NULL) return ;
+		while (phead) {
+			std::cout << phead->val << " ";
+			phead = phead->next;
+		}
+		std::cout << std::endl;
+	}
+};
+
+ListNode* init() {
+	ListNode *node4 = new ListNode(4, NULL);
+	ListNode *node3 = new ListNode(3, node4);
+	ListNode *node2 = new ListNode(2, node3);
+	ListNode *node1 = new ListNode(1, node2);
+
+	ListNode *phead = node1;
+	return phead;
+}
+
+void test_01() {
+	ListNode* phead =  init();
+	Solution test;
+	std::cout << "before:" << std::endl;
+	test.traval(phead);
+
+	phead = test.reverse(phead);
+
+	std::cout << "after:" << std::endl;
+	test.traval(phead);
+	std::cout << std::endl;
+}
+
+void test_02() {
+	ListNode* phead = init();
+	Solution test;
+	std::cout << "way 2 :" << std::endl;
+	std::cout << "before:" << std::endl;
+	test.traval(phead);
+
+	phead = test.reverselist(phead);
+	std::cout << "after:" << std::endl;
+	test.traval(phead);
+	std::cout << std::endl;
+}
+
+void test_03() {
+	ListNode* phead = init();
+	Solution test;
+
+	std::cout << "Way 3:" << std::endl;
+	std::cout << "before:" << std::endl;
+	test.traval(phead);
+
+	phead = test.recursion_reverselist(phead);
+	std::cout << "after:" << std::endl;
+	test.traval(phead);
+}
+
+int main() {
+	test_01();
+	test_02();
+	test_03();
+}
