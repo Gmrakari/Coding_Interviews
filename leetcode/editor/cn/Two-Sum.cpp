df51468e8434fe0db60b685c187a6ad7639cad7d
Gmrakari <gmrakari@outlook.com>
2022-05-16 11:22:44 +0800

    [add]use unordered_map method to realize

--- leetcode/editor/cn/Two-Sum.cpp
+++ leetcode/editor/cn/Two-Sum.cpp
@@ -12,19 +12,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        if (nums.size() < 0) return {};
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 
 void test() {
-    cout << "Begin\n" << endl;
+    cout << "Begin" << endl;
     
     Solution s;
     vector<int> vec{7, 1, 5, 3, 6, 4};
+    int target = 6;
+
+    vector<int> ret = s.twoSum(vec, target);
+
+    for (auto i : ret) {
+        cout << i << " ";
+    }
+    cout <<endl;
     
-    cout << "End\n" << endl;
+    cout << "End" << endl;
 }
 
 
