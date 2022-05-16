10ddd3733e9f0f031f2d176158d2496996d964d8
Gmrakari <gmrakari@outlook.com>
2022-05-16 11:09:25 +0800

    [add]headers and source files

---
+++ leetcode/editor/cn/Two-Sum.cpp
@@ -0,0 +1,34 @@
+/*
+*
+** @filename:1Two Sum
+** @date:2022-05-16 10:24:09
+*  @author:gmrakari
+*
+*/
+
+#include "include/headers.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+void test() {
+    cout << "Begin\n" << endl;
+    
+    Solution s;
+    vector<int> vec{7, 1, 5, 3, 6, 4};
+    
+    cout << "End\n" << endl;
+}
+
+
+int main()
+{
+    test();
+}
---
+++ leetcode/editor/cn/include/headers.h
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @filename:headers.h
+ * @function:
+ * @return:
+ * @author:Zhanyou Zhou (gmrakari@outlook.com)
+ * @date:2022/5/16 10:01
+ * 
+ */
+
+#ifndef CODING_INTERVIEWS_HEADERS_H
+#define CODING_INTERVIEWS_HEADERS_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#endif //CODING_INTERVIEWS_HEADERS_H
