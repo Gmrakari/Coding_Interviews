d879df14a4c4a0e6769479c8f9cb68e33d7dd4f2
Gmrakari <gmrakari@outlook.com>
2022-05-25 10:37:42 +0800

    [ADD]Heap Sort

--- leetcode/editor/cn/Sort-an-Array.cpp
+++ leetcode/editor/cn/Sort-an-Array.cpp
@@ -40,6 +40,50 @@ public:
             quicksort(nums, pv + 1, high);
         }
     }
+
+    vector<int> sortArra_Heap(vector<int>& nums) {
+        if (nums.size() == 0) return {};
+        vector<int> ret = nums;
+        heap_sort(ret, ret.size());
+        return ret;
+    }
+
+/*
+* 算法步骤
+* 1、创建一个堆Heap[0···n - 1]
+* 2、把堆首(最大值)和堆尾交换
+* 3、把堆的尺寸缩小1，兵调用shift_down(0)，目的是把新的数组顶端数据调整到相应位置
+* 4、重复步骤2，直到堆的尺寸为1
+*/
+
+    void MaxHeapify(vector<int>& nums, int start, int end) {
+        int son = start;
+        int dad = son * 2 + 1;
+        while (son <= end) {
+            if (son + 1 <= end && nums[son] < nums[son + 1])
+                son++;
+            if (nums[dad] > nums[son])
+                return ;
+            else {
+                swap(nums[son], nums[dad]);
+                dad = son;
+                son = dad * 2 + 1;
+            }
+        }
+    }
+
+    int heap_sort(vector<int>& nums, int len) {
+        //初始化
+        for (int i = len / 2 - 1;i >= 0; i--) {
+            MaxHeapify(nums, i, len - 1);
+        }
+
+        //最后一个元素和堆顶交换
+        for (int i = len - 1; i > 0; i--) {
+            swap(nums[0], nums[i]);
+            MaxHeapify(nums, 0, i - 1);
+        }
+    }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
@@ -58,6 +102,19 @@ void test() {
     cout << "\nEnd" << endl;
 }
 
+void test_2() {
+    cout << "Begin" << endl;
+
+    Solution s;
+    vector<int> vec{7, 1, 5, 3, 6, 4};
+    vector<int> ret = s.sortArra_Heap(vec);
+
+    for (auto i : ret) {
+        cout << i << " ";
+    }
+
+    cout << "\nEnd" << endl;
+}
 
 int main()
 {
